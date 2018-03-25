3fbc2f6bc38919f1c54fd76cb35fb4de0423e9de
Fausto Rendon <faustor001@gmail.com>
2018-03-25 14:38:33 -0700

    Add new post functionality with redux form

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostIndex extends Component {
     //React life cycle method
@@ -22,6 +23,11 @@ class PostIndex extends Component {
     render() {
         return(
             <div>
+                <div  className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Post
+                    </Link>
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
